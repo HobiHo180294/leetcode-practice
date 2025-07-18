eeb6d2685dcb21520b3dfd648723e40742388a66
Yehor Potebenko <91216619+HobiHo180294@users.noreply.github.com>
2025-07-18 20:21:06 +0200

    Time: 61 ms (6.12%) | Memory: 56.9 MB (5.05%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -2,7 +2,7 @@
  * @param {string} word
  * @return {number}
  */
-var possibleStringCount = function (word) {
+const possibleStringCount = function (word) {
     const MINIMAL_GROUP_LENGTH = 1;
     const CONSECUTIVE_CHAR_GROUP_REGEX = /(.)\1*/g;
 
