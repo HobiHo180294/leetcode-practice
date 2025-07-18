f6d7e105efb93b57ddb575d002efa6507a36ca1b
Yehor Potebenko <91216619+HobiHo180294@users.noreply.github.com>
2025-07-18 20:24:10 +0200

    Time: 53 ms (29.97%) | Memory: 57.3 MB (5.05%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -2,15 +2,19 @@
  * @param {string} word
  * @return {number}
  */
-const possibleStringCount = function (word) {
-    const MINIMAL_GROUP_LENGTH = 1;
-    const CONSECUTIVE_CHAR_GROUP_REGEX = /(.)\1*/g;
+function getPossibleStringCount() {
+  const MINIMAL_GROUP_LENGTH = 1;
+  const CONSECUTIVE_CHAR_GROUP_REGEX = /(.)\1*/g;
 
+  return function (word) {
     return (
-        MINIMAL_GROUP_LENGTH +
-        word
-            .match(CONSECUTIVE_CHAR_GROUP_REGEX)
-            .map(group => group.length - MINIMAL_GROUP_LENGTH)
-            .reduce((total, variantsFromGroup) => total + variantsFromGroup, 0)
+      MINIMAL_GROUP_LENGTH +
+      word
+        .match(CONSECUTIVE_CHAR_GROUP_REGEX)
+        .map(group => group.length - MINIMAL_GROUP_LENGTH)
+        .reduce((total, variantsFromGroup) => total + variantsFromGroup, 0)
     );
-};
+  };
+}
+
+const possibleStringCount = getPossibleStringCount();
