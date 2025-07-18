afb9ea11d4d0114597dedd0509c7bee0946553c6
Yehor Potebenko <91216619+HobiHo180294@users.noreply.github.com>
2025-07-18 20:00:42 +0200

    Time: 55 ms (20.44%) | Memory: 55.8 MB (8.93%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -3,7 +3,11 @@
  * @return {number}
  */
 var possibleStringCount = function (word) {
-    return 1 + word.match(/(.)\1*/g)
-        .map(g => g.length - 1)
-        .reduce((sum, x) => sum + (x > 0 ? x : 0), 0);
+    return (
+        1 +
+        word
+            .match(/(.)\1*/g)
+            .map(group => group.length - 1)
+            .reduce((sum, x) => sum + x, 0)
+    );
 };
