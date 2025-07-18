dd5014e063d881805333d77a37d2a5a6c74d8da4
Yehor Potebenko <91216619+HobiHo180294@users.noreply.github.com>
2025-07-18 20:47:07 +0200

    Time: 49 ms (50.07%) | Memory: 56 MB (7.65%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -1,20 +1,20 @@
-/**
- * @param {string} word
- * @return {number}
- */
 function getPossibleStringCount() {
-  const MINIMAL_GROUP_LENGTH = 1;
-  const CONSECUTIVE_CHAR_GROUP_REGEX = /(.)\1*/g;
+    const MINIMAL_GROUP_LENGTH = 1;
+    const CONSECUTIVE_CHAR_GROUP_REGEX = /(.)\1*/g;
 
-  return function (word) {
-    return (
-      MINIMAL_GROUP_LENGTH +
-      word
-        .match(CONSECUTIVE_CHAR_GROUP_REGEX)
-        .map(group => group.length - MINIMAL_GROUP_LENGTH)
-        .reduce((total, variantsFromGroup) => total + variantsFromGroup, 0)
-    );
-  };
+    /**
+     * @param {string} word
+     * @return {number}
+     */
+    return function (word) {
+        return (
+            MINIMAL_GROUP_LENGTH +
+            word
+                .match(CONSECUTIVE_CHAR_GROUP_REGEX)
+                .map(group => group.length - MINIMAL_GROUP_LENGTH)
+                .reduce((total, variantsFromGroup) => total + variantsFromGroup, 0)
+        );
+    };
 }
 
 const possibleStringCount = getPossibleStringCount();
