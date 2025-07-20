972375845c1b879da373e7f784076b59861583ac
Yehor Potebenko <91216619+HobiHo180294@users.noreply.github.com>
2025-07-20 11:51:58 +0200

    Time: 6 ms (35.71%) | Memory: 57.2 MB (43.63%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.js
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.js
@@ -1,27 +1,27 @@
 function getNextWordPart(word) {
-  let next = '';
+    let next = '';
 
-  for (const char of word) {
-    next += String.fromCharCode(char.charCodeAt(0) + 1);
-  }
+    for (const char of word) {
+        next += String.fromCharCode(char.charCodeAt(0) + 1);
+    }
 
-  return word + next;
+    return word + next;
 }
 
 function getkthCharacter() {
-  /**
-   * @param {number} k
-   * @return {character}
-   */
-  return function (k) {
-    let word = 'a';
+    /**
+     * @param {number} k
+     * @return {character}
+     */
+    return function (k) {
+        let word = 'a';
 
-    while (word.length < k) {
-      word = getNextWordPart(word);
-    }
+        while (word.length < k) {
+            word = getNextWordPart(word);
+        }
 
-    return word[k - 1];
-  };
+        return word[k - 1];
+    };
 }
 
 const kthCharacter = getkthCharacter();
